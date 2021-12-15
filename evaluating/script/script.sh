cf35fe98f531691e9625d0be8b09a2d8e29266de
Valentin HEMERET <vahemere@e3r11p11.42.fr>
2021-12-15 19:52:18 +0100

    before eval

--- evaluating/script/script.sh
+++ evaluating/script/script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo -ne "#Architecture: "; uname -a
-echo -ne "#CPU physical : "; grep -c ^processor /proc/cpuinfo
-echo -ne "#vCPU : "; cat /proc/cpuinfo | grep processor | wc -l
-echo -ne "#Memory Usage: "; free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-echo -ne "#Disk Usage: "; df -h | awk '$NF=="/"{printf "%d/%dGB (%s)\n", $3,$2,$5}'
-echo -ne "#CPU load: "; top -bn1 | grep load | awk '{printf "%.2f%%\n", $(NF-2)}'
-echo -ne "#Last boot: "; who | awk '{print $3}' | tr '\n' ' ' && who | awk '{print $4}'
-echo -ne "#LVM use: "; if cat /etc/fstab | grep -q "/dev/mapper/"; then echo "yes"; else echo "no"; fi
-echo -ne "#Connexions TCP : "; cat /proc/net/tcp | wc -l | awk '{print $1-1}' | tr '\n' ' ' && echo "ESTABLISHED"
-echo -ne "#User log : "; w | wc -l | awk '{print$1-2}'
-echo -ne "#Network : "; echo -n "IP " && ip route list | grep link | awk '{print $9}' | tr '\n' ' ' && echo -n "(" && ip link show | grep link/ether | awk '{print $2}' | tr '\n' ')' && printf "\n"
-echo -ne "#Sudo : "; cat /var/log/sudo/log | wc -l | tr '\n' ' ' && echo "cmd"
+echo -n "#Architecture: "; uname -a
+echo -n "#CPU physical : "; grep -c processor /proc/cpuinfo
+echo -n "#vCPU : "; cat /proc/cpuinfo | grep processor | wc -l
+echo -n "#Memory Usage: "; free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+echo -n "#Disk Usage: "; df -h | awk 'NR==4{printf "%d/%dGb (%d%%)\n", $3,$2,$3*100/$2}'
+echo -n "#CPU load: "; top -bn1 | grep load | awk '{printf "%.2f%%\n", $11}'
+echo -n "#Last boot: "; who | awk '{printf"%s %s\n", $3, $4}'
+echo -n "#LVM use: "; if lsblk | grep -q lvm; then echo "yes"; else echo "no"; fi
+echo -n "#Connexions TCP : "; netstat -ant | grep tcp | wc -l | awk '{printf"%d ESTABLISHED\n", $1-1}'
+echo -n "#User log : "; w | awk 'NR==1{printf"%d\n", $4}'
+echo -n "#Network : "; hostname -I | awk '{printf"IP %s ", $1}' && ip addr | awk 'NR==8{printf"(%s) \n", $2}'
+echo -n "#Sudo : "; grep -c "COMMAND=" /var/log/sudo/log
 printf "\n"
