25bec2a0f7af5ab5dc0d65f902f5a69dc5b79bed
Valentin HEMERET <vahemere@e3r11p11.42.fr>
2021-12-16 11:03:13 +0100

    i think it's finish

--- evaluating/script/script.sh
+++ evaluating/script/script.sh
@@ -8,8 +8,8 @@ echo -n "#Disk Usage: "; df -h | awk 'NR==4{printf "%d/%dGb (%d%%)\n", $3,$2,$3*
 echo -n "#CPU load: "; top -bn1 | grep load | awk '{printf "%.2f%%\n", $11}'
 echo -n "#Last boot: "; who | awk '{printf"%s %s\n", $3, $4}'
 echo -n "#LVM use: "; if lsblk | grep -q lvm; then echo "yes"; else echo "no"; fi
-echo -n "#Connexions TCP : "; netstat -ant | grep tcp | wc -l | awk '{printf"%d ESTABLISHED\n", $1-1}'
-echo -n "#User log : "; w | awk 'NR==1{printf"%d\n", $4}'
+echo -n "#Connexions TCP : "; netstat -a | grep tcp | wc -l | awk '{printf"%d ESTABLISHED\n", $1-1}'
+echo -n "#User log : "; w | awk 'NR==1{printf"%d\n", $5}'
 echo -n "#Network : "; hostname -I | awk '{printf"IP %s ", $1}' && ip addr | awk 'NR==8{printf"(%s) \n", $2}'
 echo -n "#Sudo : "; grep -c "COMMAND=" /var/log/sudo/log
 printf "\n"
