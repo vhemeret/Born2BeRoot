ab32c1836da0434fc48a19f351b14e1daa5fa18e
vhemeret <val.kawa@icloud.com>
2021-12-17 21:22:42 +0100

    disk usage fixed

--- evaluating/script/script.sh
+++ evaluating/script/script.sh
@@ -4,7 +4,7 @@ echo -n "#Architecture: "; uname -a
 echo -n "#CPU physical : "; grep -c processor /proc/cpuinfo
 echo -n "#vCPU : "; cat /proc/cpuinfo | grep processor | wc -l
 echo -n "#Memory Usage: "; free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-echo -n "#Disk Usage: "; df -h | awk 'NR==4{printf "%d/%dGb (%d%%)\n", $3,$2,$3*100/$2}'
+echo -n "#Disk Usage: "; df -m | awk 'NR==4{printf "%d", $3}' && df -h | awk 'NR==4{printf"/%dGb (%d%%)\n", $2, $3*100/$2}'
 echo -n "#CPU load: "; top -bn1 | grep load | awk '{printf "%.2f%%\n", $11}'
 echo -n "#Last boot: "; who | awk '{printf"%s %s\n", $3, $4}'
 echo -n "#LVM use: "; if lsblk | grep -q lvm; then echo "yes"; else echo "no"; fi
